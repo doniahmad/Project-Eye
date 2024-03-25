d1f0f478ec94eccc2cc059bf13c20017dcc7854c
Yusrizall <yusrizalll36@gmail.com>
2024-03-25 13:54:07 +0700

    [YN] : Fix movement character.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
-    private PlayerInput.OnFootActions onFoot;
+    private PlayerInput.OnFootActions player;
 
     private PlayerMotor motor;
     private PlayerLook look;
@@ -15,32 +15,40 @@ public class InputManager : MonoBehaviour
     void Awake()
     {
         playerInput = new PlayerInput();
-        onFoot = playerInput.OnFoot;
+        player = playerInput.OnFoot;
 
         motor = GetComponent<PlayerMotor>();
         look = GetComponent<PlayerLook>();
         interact = GetComponent<PlayerInteract>();
 
-        onFoot.Jump.performed += ctx => motor.Jump();
-        onFoot.Interact.performed += ctx => interact.OnInteractAction();
+        player.Jump.performed += ctx => motor.Jump();
+        player.Interact.performed += ctx => interact.OnInteractAction();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //tell the playermotor to move using the value from movement action.
-        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        motor.ProcessMove(GetVector2Normalized());
     }
     private void LateUpdate()
     {
-        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+        look.ProcessLook(player.Look.ReadValue<Vector2>());
+    }
+
+    private Vector2 GetVector2Normalized() {
+        Vector2 inputVector = player.Movement.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
     }
     private void OnEnable()
     {
-        onFoot.Enable();
+        player.Enable();
     }
     private void OnDisable()
     {
-        onFoot.Disable();
+        player.Disable();
     }
 }
